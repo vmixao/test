d72743363c6ae50c761162bf633de1aaf361df61
Verónica Mixão <veronica.mixao@insa.min-saude.pt>
2022-07-01 18:43:50 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="mutprofile",
+    version="0.1.1",
+    author="V Mixao",
+    author_email="vmixao@gmail.com",
+    description="This is my first test,
+    url="https://github.com/vmixao/test",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ),
+)
